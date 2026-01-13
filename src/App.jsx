e5bed519208b6767bdf8fadb7019386a979e13e4
Alex Boschuk <bostchuk@gmail.com>
2026-01-13 12:15:12 -0500

    Publish Porfolio Page Top Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 import Home from "./pages/Home.jsx";
 import JavaPlaywright from "./pages/JavaPlaywright.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-
-      {/* Presentation page */}
-      <Route path="/java-playwright" element={<JavaPlaywright />} />
-
-      {/* fallback so blank pages don’t happen */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/java-playwright" element={<JavaPlaywright />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
